a8da9917f76f3c3bf0441104592a67c76bb656b4
isanf <issak.sanf@gmail.com>
2021-12-21 19:47:16 +0100

    Unit Test Matching Algo

--- testapis/testapis_test.go
+++ testapis/testapis_test.go
@@ -7,48 +7,108 @@ import (
 	"goanonnonces.leboncoin.fr/handler"
 )
 
+// "rs4 avant", l'annonce devra afficher "Audi" et "Rs4"
+func TestMatchingAlgo1(t *testing.T){
+	saisie := "rs4 avant"
+	idbrand := handler.MatchingAlgo(saisie)
 
-func TestReadAnnonceByID(t *testing.T) {
+	// La marque Audi a l'identifiant "1"
+	expected := 1
 
-	req, err := http.NewRequest("GET", "/annoces/1", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handler.ReadAnnonce)
-	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status != http.StatusOK {
+	if idbrand != expected {
 		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	// Check the response body is what we expect.
-	expected := `{"type":"succès","data":[{"id":"1","titre":"Voiture 1","contenu":"En vente à bon prix 1","categorie":"1","model":"1"}],"message":"L'Annonce a été bien recupérée!"}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		idbrand, expected)
 	}
 }
 
+// "Gran Turismo Série5", l'annonce devra afficher "BMW" et "Serie 5"
+func TestMatchingAlgo2(t *testing.T){
+	saisie := "Gran Turismo Série5"
+	idbrand := handler.MatchingAlgo(saisie)
+
+	// La marque BMW a l'identifiant "2"
+	expected := 2
+
+	if idbrand != expected {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+		idbrand, expected)
+	}
+}
+
+// "ds 3 crossback", l'annonce devra afficher "Citroen" et "Ds3"
+func TestMatchingAlgo3(t *testing.T){
+	saisie := "ds 3 crossback"
+	idbrand := handler.MatchingAlgo(saisie)
+
+	// La marque Citroen a l'identifiant "3"
+	expected := 3
+
+	if idbrand != expected {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+		idbrand, expected)
+	}
+}
+
+// "CrossBack ds 3", l'annonce devra afficher "Citroen" et "Ds3"
+func TestMatchingAlgo4(t *testing.T){
+	saisie := "CrossBack ds 3"
+	idbrand := handler.MatchingAlgo(saisie)
+
+	// La marque Citroen a l'identifiant "3"
+	expected := 3
+
+	if idbrand != expected {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+		idbrand, expected)
+	}
+}
 
 func TestGetAnnonces(t *testing.T) {
-	req, err := http.NewRequest("GET", "/entries", nil)
+	req, err := http.NewRequest("GET", "/annonces", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(handler.GetAnnonces)
 	handler.ServeHTTP(rr, req)
+	// Verifier si la reponse est OK
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got = %v want = %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestReadAnnonceByID(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/annonces/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler.ReadAnnonce)
+	handler.ServeHTTP(rr, req)
+	// Verifier si la reponse est OK
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
-	// Check the response body is what we expect.
-	expected := `{"type":"success","data":[{"id":"1","titre":"Voiture 1","contenu":"En vente à bon prix 1","categorie":"1","model":"1"},{"id":"2","titre":"Voiture 2","contenu":"En vente à bon prix 2","categorie":"2","model":"2"},{"id":"3","titre":"Voiture 3","contenu":"En vente à bon prix 3","categorie":"3","model":"3"}],"message":"La liste des Annonces"}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+}
+
+
+func TestDeleteByID(t *testing.T) {
+
+	req, err := http.NewRequest("DELETE", "/annonces/1", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler.ReadAnnonce)
+	handler.ServeHTTP(rr, req)
+	// Verifier si la reponse est OK
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
 }
